6133e7e3793d104965d17d5414798ad82ff17c48
MehemmedHajyev <hajyeffmehemmed8@gmail.com>
2022-09-29 02:43:00 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,186 @@
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  centeredSlides: true,
+  initialSlide: 2,
+  // init: false,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: false,
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    350: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+
+    },
+    540: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+
+    },
+    1100: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+
+    },
+  }
+});
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  slidesPerGroup: 5,
+  loop: true,
+  loopFillGroupWithBlank: false,
+  breakpoints: {
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    1640: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  centeredSlides: true,
+  initialSlide: 2,
+  // init: false,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: false,
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    350: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+
+    },
+    540: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+
+    },
+    1100: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+
+    },
+  }
+});
+
+var swiper = new Swiper('.mySwiper3', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  centeredSlides: true,
+  initialSlide: 2,
+  // init: false,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    350: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+
+    },
+    540: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+
+    },
+    1100: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+
+    },
+  }
+
+
+});
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,7 @@
+
+// let headerUl = document.querySelector('.header_ul');
+// function func() {
+//     headerUl.classList.toggle('acttt')
+//     // headerUl.style.transition='1s'
+//     style.cssText ="-webkit-transition: all 0.5s ease-in-out;";
+// }
